6dbbd4f721ede53e390eda92a3f574d59822951f
ieatmosquitos <fabrizio.nenci@gmail.com>
2013-07-18 18:34:37 +0200

    Faster, stronger and more beautiful.

--- src/condenser.cpp
+++ src/condenser.cpp
@@ -221,65 +221,76 @@ void orderLastElement(std::vector<g2o::EdgeSE3 *> &poses){
 }
 
 
-int purgeLandmarks(Star3D * s,g2o::SparseOptimizer  * optimizer){
-  unsigned int dropped = 0;
+unsigned int confirmLandmarks(VertexWrapper ** candidates, g2o::OptimizableGraph::Edge *** cand_edges, unsigned int cand_number, unsigned int * cand_edges_number, Star3D * star, g2o::SparseOptimizer  * optimizer){
   
-  // push the star state
-  s->pushState();
+  // std::cout << "cand number: " << cand_number << std::endl;
+  
+  unsigned int kept = 0;
+  
+  // push all the vertices
+  for(unsigned int i=0; i<cand_number; i++){
+    candidates[i]->vertex->push();
+  }
+  for(unsigned int i=0; i<star->poses.size(); i++){
+    star->poses[i]->vertex->push();
+  }
+  
   
   g2o::OptimizableGraph::EdgeSet toOptimize;
-  for(unsigned int i=0; i<s->edgesLandmarks.size(); i++){
-    toOptimize.insert(s->edgesLandmarks[i]);
-  }
-  for(unsigned int i=0; i<s->edgesPoses.size(); i++){
-    toOptimize.insert(s->edgesPoses[i]);
+  for(unsigned int i=0; i<star->edgesPoses.size(); i++){
+    toOptimize.insert(star->edgesPoses[i]);
   }
   
   optimizer->initializeOptimization(toOptimize); 
   optimizer->computeInitialGuess();
   
-  toOptimize.clear();
+  // fix the poses
+  for(unsigned int i=0; i<star->poses.size(); i++){
+    star->poses[i]->vertex->setFixed(true);
+  }
   
-  for(unsigned int l=0; l<s->landmarks.size(); l++){
-    s->pushState();
+  // check every landmark for stability
+  for(unsigned int c=0; c<cand_number; c++){
+    //    std::cerr << "accessing candidate " << c << std::endl;
+    VertexWrapper * vw = candidates[c];
+    bool accept_it = false;
     
-    VertexWrapper * vw = s->landmarks[l];
-    bool remove_it = false;
-    
-    std::vector<g2o::OptimizableGraph::Vertex *> toFix;
-    
-    // look for poses in the star that see this landmark
-    for(unsigned int e=0; e<vw->edges.size(); e++){
-      
-      if(s->contains(vw->edges[e]->vertices()[0])){
-	toOptimize.insert(vw->edges[e]);
-	toFix.push_back((g2o::OptimizableGraph::Vertex *) (vw->edges[e]->vertices()[0]));
-      }
+    //    std::cerr << "check min obs" << std::endl;
+    //    std::cerr << "edges number: " << cand_edges_number[c] << std::endl;
+    if(cand_edges_number[c] < _minimum_observations){
+      continue;
     }
     
-    // fix those poses
-    for(unsigned int i=0; i<toFix.size(); i++){
-      toFix[i]->setFixed(true);
-    }
+    //    std::cerr << "clear toOptimize" << std::endl;
+    toOptimize.clear();
     
-    // solve the landmark
+    for(unsigned int e=0; e<cand_edges_number[c]; e++){
+      toOptimize.insert(cand_edges[c][e]);
+    }
+    //    std::cerr << "initializeOptim..." << std::endl;
     optimizer->initializeOptimization(toOptimize);
+    //    std::cerr << "solver init" << std::endl;
     optimizer->solver()->init();
+    //    std::cerr << "build linear struct" << std::endl;
     if (!solverWithHessian->buildLinearStructure()){
       std::cerr << "FATAL: failure while building linear structure" << std::endl;
       exit(25);
     }
+    //    std::cerr << "errors..." << std::endl;
     optimizer->computeActiveErrors();
+    //    std::cerr << "update" << std::endl;
     solverWithHessian->updateLinearSystem();
     
+    //    std::cerr << "solve direct" << std::endl;
     vw->vertex->solveDirect();
     
     g2o::OptimizableGraph::Vertex * v = vw->vertex;
     
+    //    std::cerr << "hessian stuff" << std::endl;
     Eigen::MatrixXd h(v->dimension(), v->dimension());
     for (int i=0; i<v->dimension(); i++){
       for (int j=0; j<v->dimension(); j++)
-	h(i,j)=v->hessian(i,j);
+    	h(i,j)=v->hessian(i,j);
     }
     Eigen::EigenSolver<Eigen::MatrixXd> esolver;
     esolver.compute(h);
@@ -290,43 +301,155 @@ int purgeLandmarks(Star3D * s,g2o::SparseOptimizer  * optimizer){
       emin = ev(i).real()>emin ? emin : ev(i).real();
       emax = ev(i).real()<emax ? emax : ev(i).real();
     }
-    double d=emin/emax;
+    double eigen_ratio=emin/emax;
+    //    std::cerr << "ratio computed" << std::endl;
     
-    if(d<1e-3){
-      //      std::cout << "d = " << d << std::endl;
-      remove_it = true;
+    if(eigen_ratio > 1e-3){
+      accept_it = true;
     }
     
-    
-    if(remove_it){
-      dropped++;
-      // should remove this landmark from the star
-       
-      // search the edges in the star that leads to this landmark
-      for(unsigned int i=0; i<s->edgesLandmarks.size(); i++){
-    	if(s->edgesLandmarks[i]->vertices()[1] == vw->vertex){
-    	  s->edgesLandmarks.erase(s->edgesLandmarks.begin()+i);
-    	  i--;
-    	}
+    if(accept_it){
+      //      std::cout << "OK...";
+      kept++;
+      // add the landmark and its edges to the star
+      star->landmarks.push_back(candidates[c]);
+      for(unsigned int e=0; e<cand_edges_number[c]; e++){
+    	star->edgesLandmarks.push_back(cand_edges[c][e]);
       }
-  
-      s->landmarks[l]->vertex->pop();
-      s->landmarks[l]->vertex->pop();
-      s->landmarks.erase(s->landmarks.begin()+l);
-      l--;
-       
+      //      std::cout << "Accepted" << std::endl;
+    }
+    else{
+      //      std::cerr << "BUUUUUUUUUUUU!" << std::endl;
     }
     
-    for(unsigned int i=0; i<toFix.size(); i++){
-      toFix[i]->setFixed(false);
-    }
-    
-    s->popState();
+    //    std::cerr << "next please..." << std::endl;
   }
- 
-  s->popState();
-  return dropped;
+  
+  //  std::cerr << "unfixing" << std::endl;
+  // unfix poses
+  for(unsigned int i=0; i<star->poses.size(); i++){
+    star->poses[i]->vertex->setFixed(false);
+  }
+  
+  //  std::cerr << "popping poses" << std::endl;
+  // pop all the vertices
+  for(unsigned int i=0; i<star->poses.size(); i++){
+    star->poses[i]->vertex->pop();
+  }
+  //  std::cerr << "popping landmarks" << std::endl;
+  for(unsigned int i=0; i<cand_number; i++){
+    candidates[i]->vertex->pop();
+  }
+  
+  return kept;
 }
+
+// int purgeLandmarks(Star3D * s,g2o::SparseOptimizer  * optimizer){
+//   unsigned int dropped = 0;
+  
+//   // push the star state
+//   s->pushState();
+  
+//   g2o::OptimizableGraph::EdgeSet toOptimize;
+//   for(unsigned int i=0; i<s->edgesLandmarks.size(); i++){
+//     toOptimize.insert(s->edgesLandmarks[i]);
+//   }
+//   for(unsigned int i=0; i<s->edgesPoses.size(); i++){
+//     toOptimize.insert(s->edgesPoses[i]);
+//   }
+  
+//   optimizer->initializeOptimization(toOptimize); 
+//   optimizer->computeInitialGuess();
+  
+//   toOptimize.clear();
+  
+//   for(unsigned int l=0; l<s->landmarks.size(); l++){
+//     s->pushState();
+    
+//     VertexWrapper * vw = s->landmarks[l];
+//     bool remove_it = false;
+    
+//     std::vector<g2o::OptimizableGraph::Vertex *> toFix;
+    
+//     // search the star for poses that see this landmark
+//     for(unsigned int e=0; e<vw->edges.size(); e++){
+      
+//       if(s->contains(vw->edges[e]->vertices()[0])){
+// 	toOptimize.insert(vw->edges[e]);
+// 	toFix.push_back((g2o::OptimizableGraph::Vertex *) (vw->edges[e]->vertices()[0]));
+//       }
+//     }
+    
+//     // fix those poses
+//     for(unsigned int i=0; i<toFix.size(); i++){
+//       toFix[i]->setFixed(true);
+//     }
+    
+//     // solve the landmark
+//     optimizer->initializeOptimization(toOptimize);
+//     optimizer->solver()->init();
+//     if (!solverWithHessian->buildLinearStructure()){
+//       std::cerr << "FATAL: failure while building linear structure" << std::endl;
+//       exit(25);
+//     }
+//     optimizer->computeActiveErrors();
+//     solverWithHessian->updateLinearSystem();
+    
+//     vw->vertex->solveDirect();
+    
+//     g2o::OptimizableGraph::Vertex * v = vw->vertex;
+    
+//     Eigen::MatrixXd h(v->dimension(), v->dimension());
+//     for (int i=0; i<v->dimension(); i++){
+//       for (int j=0; j<v->dimension(); j++)
+// 	h(i,j)=v->hessian(i,j);
+//     }
+//     Eigen::EigenSolver<Eigen::MatrixXd> esolver;
+//     esolver.compute(h);
+//     Eigen::VectorXcd ev= esolver.eigenvalues();
+//     double emin = std::numeric_limits<double>::max();
+//     double emax = -std::numeric_limits<double>::max();
+//     for (int i=0; i<ev.size(); i++){
+//       emin = ev(i).real()>emin ? emin : ev(i).real();
+//       emax = ev(i).real()<emax ? emax : ev(i).real();
+//     }
+//     double d=emin/emax;
+    
+//     if(d<1e-3){
+//       //      std::cout << "d = " << d << std::endl;
+//       remove_it = true;
+//     }
+    
+    
+//     if(remove_it){
+//       dropped++;
+//       // should remove this landmark from the star
+       
+//       // search the edges in the star that leads to this landmark
+//       for(unsigned int i=0; i<s->edgesLandmarks.size(); i++){
+//     	if(s->edgesLandmarks[i]->vertices()[1] == vw->vertex){
+//     	  s->edgesLandmarks.erase(s->edgesLandmarks.begin()+i);
+//     	  i--;
+//     	}
+//       }
+  
+//       s->landmarks[l]->vertex->pop();
+//       s->landmarks[l]->vertex->pop();
+//       s->landmarks.erase(s->landmarks.begin()+l);
+//       l--;
+       
+//     }
+    
+//     for(unsigned int i=0; i<toFix.size(); i++){
+//       toFix[i]->setFixed(false);
+//     }
+    
+//     s->popState();
+//   }
+ 
+//   s->popState();
+//   return dropped;
+// }
 
 
 void init(int argc, char** argv){
@@ -621,42 +744,13 @@ int main(int argc, char ** argv){
   { // this block requires much memory, that's why we isolate it using brackets
     Star3D * s;
     unsigned int inserted = 0;
-    VertexWrapper * candidates [100000];
-    int times [100000];
-    g2o::OptimizableGraph::Edge ** cand_edges [100000];
+    VertexWrapper * candidates [10000];
+    //int times [100000];
+    g2o::OptimizableGraph::Edge ** cand_edges [10000];
     unsigned int cand_index_next = 0;
     unsigned int cand_edges_index_next [100000];
     for(unsigned int i=0; i<poses.size(); i++){
       if(inserted==0){
-	unsigned int dropped = cand_index_next;
-	unsigned int kept = 0;
-	if(i>0){
-	  // finalize the star checking which landmarks have to be removed
-	  for(unsigned int c=0; c<cand_index_next; c++){
-	    if(times[c] >= _minimum_observations){
-	      kept ++;
-	      // confirm the landmark and its edges
-	      s->landmarks.push_back(candidates[c]);
-	      for(unsigned int ed=0; ed<cand_edges_index_next[c]; ed++){
-		s->edgesLandmarks.push_back(cand_edges[c][ed]);
-	      }
-	    }
-	  }
-	  dropped = dropped - kept;
-	
-	  // clear stuff
-	  for(unsigned int ce=0; ce<cand_index_next; ce++){
-	    free(cand_edges[ce]);
-	  }
-	
-	  cand_index_next = 0;
-	
-	
-	  dropped = dropped + purgeLandmarks(s,optimizer);
-	  std::cout << "dropped: " << dropped << "\t; kept: " << kept << std::endl;
-	  s->gauge_index = (s->poses.size()/2);
-	}
-            
 	s = new Star3D();
 	stars.push_back(s);
 	std::cout << "generating star number " << stars.size() << std::endl;
@@ -665,12 +759,12 @@ int main(int argc, char ** argv){
       if(i>0 && inserted==0){ // must add the previous pose/edge
 	i--;
       }
-    
+      
       VertexWrapper * p = poses[i];
       s->poses.push_back(p);
       s->edgesPoses.push_back(edgesPoses[i]);
       inserted = (inserted+1) % _starLength;
-    
+      
       for(unsigned int e=0; e<p->edges.size(); e++){
 	g2o::HyperGraph::Vertex * l = (g2o::HyperGraph::Vertex *) p->edges[e]->vertices()[1];
 
@@ -678,7 +772,7 @@ int main(int argc, char ** argv){
 	int wrapper_index = findWrapperIndex(candidates, cand_index_next, l);
 	if(wrapper_index != -1){
 	  vw = candidates[wrapper_index];
-	  times[wrapper_index] = times[wrapper_index] + 1;
+	  //times[wrapper_index] = times[wrapper_index] + 1;
 	  cand_edges[wrapper_index][cand_edges_index_next[wrapper_index]]=(p->edges[e]);
 	  cand_edges_index_next[wrapper_index] = cand_edges_index_next[wrapper_index] + 1 ;
 	}
@@ -689,12 +783,12 @@ int main(int argc, char ** argv){
 	    exit(15);
 	  }
 	  else{
-	    times[cand_index_next] = 1;
+	    //times[cand_index_next] = 1;
 	  
 	    candidates[cand_index_next] = vw;
-	  
+	    
 	    g2o::OptimizableGraph::Edge ** edges_vect = (g2o::OptimizableGraph::Edge **) malloc(10000*sizeof(g2o::OptimizableGraph::Edge *));
-	  
+	    
 	    //	    std::cout << "adding cand_edges..." << std::endl;
 	    cand_edges[cand_index_next] = edges_vect;
 	    edges_vect[0] = p->edges[e];
@@ -703,40 +797,28 @@ int main(int argc, char ** argv){
 	    cand_index_next ++ ;
 	  }
 	}
+      }
       
-      }
-    
-    }
-    
-    unsigned int dropped = cand_index_next;
-    unsigned int kept = 0;
-    // finalize the last star checking which landmarks have to be removed
-    for(unsigned int c=0; c<cand_index_next; c++){
-      if(times[c] >= _minimum_observations){
-	kept ++;
-	// confirm the landmark and its edges
-	stars[stars.size()-1]->landmarks.push_back(candidates[c]);
-	for(unsigned int ed=0; ed<cand_edges_index_next[c]; ed++){
-	  stars[stars.size()-1]->edgesLandmarks.push_back(cand_edges[c][ed]);
-	}
-      }
-    }
-    dropped = dropped - kept;
-	
-    // clear stuff
-    for(unsigned int ce=0; ce<cand_index_next; ce++){
-      free(cand_edges[ce]);
-    }
-	
-    cand_index_next = 0;
-	
-    dropped = dropped + purgeLandmarks(s,optimizer);
-    std::cout << "dropped: " << dropped << "\t; kept: " << kept << std::endl;
-    s->gauge_index = (s->poses.size()/2);
+      if(inserted==0 || i==poses.size()-1){
+	std::cout << "closing star " << stars.size() << std::endl;
+	std::cout << "poses in the star: " << s->poses.size() << std::endl;
+	std::cout << "landmarks (not yet confirmed):" << cand_index_next << std::endl;
+	// confirm or reject landmarks
+	unsigned int kept = confirmLandmarks(candidates, cand_edges, cand_index_next, cand_edges_index_next, s, optimizer);
+	  
+	std::cout << "dropped: " << cand_index_next - kept << "\t; kept: " << kept << std::endl;
 
+	// clear stuff
+	for(unsigned int ce=0; ce<cand_index_next; ce++){
+	  free(cand_edges[ce]);
+	}
+	  
+	cand_index_next = 0;
+	  
+	s->gauge_index = (s->poses.size()/2);
+      }
+    }
   }// end of the highly memory consuming block
-  
-  purgeLandmarks(stars[stars.size()-1], optimizer);
   
   
   std::cout << "generated " << stars.size() << " stars" << std::endl;
